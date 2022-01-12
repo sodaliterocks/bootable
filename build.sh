125fa8faf5438cbf9b2626ab6a1051433935e4c4
Ducky <ducky@zio.sh>
2022-01-12 09:11:07 +0000

    📦 various changes

---
+++ build.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+# Massive props to ACertainTopfi for the initial work to get this going!
+# (https://github.com/electricduck/sodalite/pull/10)
+
+. "$(dirname "$(realpath -s "$0")")/lib/sodaliterocks.sodalite/src/include/tools/common.sh"
+
+function update_submodules() {
+    dir="$1"
+
+    if [[ "$(ls -A $dir)" ]]; then
+        echoc "$(write_emoji "🔄")Updating submodules..."
+        git -C $dir submodule update --recursive
+    else
+        echoc "$(write_emoji "⬇️")Pulling submodules..."
+        git -C $dir submodule update --init --recursive
+    fi
+}
+
+base_dir="$(dirname "$(realpath -s "$0")")"
+variant=$1
+working_dir=$2
+ostree_repo=$3
+
+[[ -z $variant ]] && variant="custom"
+[[ -z $working_dir ]] && working_dir="$base_dir/build"
+[[ -z $ostree_repo ]] && ostree_repo="$base_dir/lib/sodaliterocks.sodalite/build/repo"
+
+update_submodules $base_dir
+update_submodules "$base_dir/lib/sodaliterocks.sodalite"
+
+if [[ -d $ostree_repo ]]; then
+    ostree summary -v --repo "$ostree_repo" > /dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        echoc error "Not an OSTree repository ($ostree_repo)"
+        exit
+    fi
+else
+    echoc error "OSTree repository does not exist ($ostree_repo)"
+    exit
+fi
+
+test_root
+
+if [[ -d $working_dir ]]; then
+    echoc "$(write_emoji "🗑️")Removing old build..."
+    rm -rf $working_dir
+fi
+
+orig_dir=$(pwd)
+
+mkdir -p $working_dir
+chown -R root:root $working_dir
+cd $working_dir
+
+iso_product="Sodalite"
+iso_version="stable" # TODO: Get this from the OSTree build
+iso_version_base="35" # TODO: Get this from the OSTree build
+iso_version_release="$(date +"%Y%m%d")"
+iso_variant=$variant # TODO: Get this from the OSTree build
+iso_arch="x86_64" # TODO: Get this from the OSTree build
+iso_ostree_oskey="${iso_product,,}-$iso_version_base-${iso_variant,,}"
+iso_ostree_repo_install=$ostree_repo
+iso_ostree_repo_update="https://ostree.zio.sh/repo"
+iso_ostree_ref_install="sodalite/stable/x86_64/$variant" # TODO: Get this from the OSTree build
+iso_ostree_ref_update=$iso_ostree_ref_install
+
+echoc "$(write_emoji "📀")Building ISO (this will take a while)..."
+exec lorax --product=$iso_product \
+            --version=$iso_version_base \
+            --source=https://kojipkgs.fedoraproject.org/compose/$iso_version_base/latest-Fedora-$iso_version_base/compose/Everything/$iso_arch/os/ \
+            --variant=$iso_variant \
+		    --release=$iso_release \
+            --nomacboot \
+            --volid="$iso_product-$iso_arch-$iso_version_base-$iso_release" \
+            --add-template=$base_dir/lib/fedora-lorax-templates/ostree-based-installer/lorax-configure-repo.tmpl \
+            --add-template=$base_dir/lib/fedora-lorax-templates/ostree-based-installer/lorax-embed-repo.tmpl \
+            --add-template-var=ostree_install_repo=file://$iso_ostree_repo_install \
+            --add-template-var=ostree_update_repo=$iso_ostree_repo_update \
+            --add-template-var=ostree_osname=fedora \
+            --add-template-var=ostree_oskey=$iso_ostree_oskey \
+            --add-template-var=ostree_install_ref=$iso_ostree_ref_install \
+            --add-template-var=ostree_update_ref=$iso_ostree_ref_update \
+            --logfile=$working_dir/lorax.log \
+            --tmp=$working_dir/tmp \
+            --rootfs-size=8 \
+            $working_dir/output
+
+            # BUG: Fails with "Can't check signature: public key not found"
+            #--add-template=$base_dir/lib/fedora-lorax-templates/ostree-based-installer/lorax-embed-flatpaks.tmpl \
+            #--add-template-var=flatpak_remote_name="AppCenter" \
+            #--add-template-var=flatpak_remote_url=https://flatpak.elementary.io/repo/ \
+            #--add-template-var=flatpak_remote_refs="io.elementary.Platform/x86_64/6.1" \
+
+echoc "$(write_emoji "🛡️")Correcting permissions for build directory..."
+real_user=$(get_sudo_user)
+chown -R $real_user:$real_user $working_dir
+
+cd $orig_dir
